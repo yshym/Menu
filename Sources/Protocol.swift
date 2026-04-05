4b621ab1d1732fe5ca6b5610a740ae4b58254896
Yevhen Shymotiuk <yshym@pm.me>
2026-04-05 21:17:29 +0300

    Update socket and log path to /tmp

--- Sources/Protocol.swift
+++ Sources/Protocol.swift
@@ -9,16 +9,10 @@ enum MenuProtocol {
     static let respOk = "ok"
 
     static func socketPath() -> String {
-        if let tmpdir = ProcessInfo.processInfo.environment["TMPDIR"] {
-            return "\(tmpdir)/menu.sock"
-        }
-        return "/tmp/menu-\(getuid()).sock"
+        "/tmp/menu-\(getuid()).sock"
     }
 
     static func logPath() -> String {
-        if let tmpdir = ProcessInfo.processInfo.environment["TMPDIR"] {
-            return "\(tmpdir)/menu-server.log"
-        }
-        return "/tmp/menu-server-\(getuid()).log"
+        "/tmp/menu-server-\(getuid()).log"
     }
 }
